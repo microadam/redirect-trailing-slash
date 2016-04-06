4785bff983d12d232ef882f40af8f234ce67fa8c
Ben Gourley <bn@grly.me>
2016-04-06 12:41:49 +0100

    Rewrite to fix open redirect vulnerability

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 module.exports = middleware
 
-// Speeds up page loading times
-// http://webdesign.about.com/od/beginningtutorials/f/why-urls-end-in-slash.htm
 function middleware(req, res, next) {
-  if (/\w+\/$/.test(req.url)) {
-    res.redirect(req.url.substr(0, req.url.length - 1))
-  } else {
-    next()
-  }
+  if (!/\w+\/$/.test(req.url)) return next()
+  var url = req.url
+    .replace(/^\/+/, '/')
+    .replace(/\/$/, '')
+  res.redirect(url)
 }
